8074079881be17e40d24c8fe6ce8e0703fe19a25
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-09-28 14:17:46 -0300

    atualizado 28/09...

--- src/pages/TransformersFive.js
+++ src/pages/TransformersFive.js
@@ -1,11 +1,103 @@
 import Container from "../components/Container"
+import Redes from "../components/layout/Redes"
+import SwitchInfor from "../components/layout/SwitchInfor"
+
+import MarkWahlberg from "../img/Atores/Mark-Wahlberg.jpeg"
+import Izabella from "../img/Atores/Isabela-Moner.jpeg"
+import VivianeWembley from "../img/Atores/Laura-Haddock.jpg"
+import JoshDuhamel from "../img/Atores/Capt.-William-Lennox.webp"
+import JohnTurturro from "../img/Atores/Agent_simmons.jpeg"
+import AnthonyHopkins from "../img/Atores/Anthony-Hopkins.jpeg"
+import JerrodCarmichael from "../img/Atores/Jerrod-Carmichael.webp"
+import SantiagoCabrera from "../img/Atores/Santiago-Cabrera.webp"
+import GeneralMorshower  from "../img/Atores/General-Morshower.jpeg"
+import StanleyTucci from "../img/Atores/Stanley-Tucci.jpeg"
 
 function TransformersFive() {
+
+    const filme = [
+        { 
+            "title": "Transformers: O Último Cavaleiro",
+            "textSinopse": "A história se desenrola em meio a uma guerra entre humanos e Transformers, que são forçados a se esconder. Cade Yeager lidera um núcleo de resistência em um ferro-velho. Enquanto isso, Optimus Prime embarca em uma jornada pelo universo até Cybertron para entender as razões de sua destruição. O filme também apresenta o vilão colossal Unicron, uma entidade robótica que devora planetas, e marca o retorno dos Dinobots."
+        }
+    ]
+
+    const atores = [
+        { 
+            "ator1" : "Mark Wahlberg", 
+            "personagem1": "como Cade Yeager", 
+            "img1": MarkWahlberg, 
+            "link1" : "https://pt.wikipedia.org/wiki/Mark_Wahlberg"
+        },
+        { 
+            "ator2" : "Isabela Moner", 
+            "personagem2": "como Izabella", 
+            "img2": Izabella , 
+            "link2" : "https://pt.wikipedia.org/wiki/Isabela_Merced"
+        },
+        { 
+            "ator3" : "Laura Haddock", 
+            "personagem3": "como Viviane Wembley", 
+            "img3": VivianeWembley, 
+            "link3" : "https://pt.wikipedia.org/wiki/Laura_Haddock"
+        },
+        { 
+            "ator4" : "Josh Duhamel", 
+            "personagem4": "como William Lenoxx" , 
+            "img4": JoshDuhamel, 
+            "link4" : "https://pt.wikipedia.org/wiki/Josh_Duhamel"
+        },
+        { 
+            "ator5" : "John Turturro", 
+            "personagem5": " como Seymour Simmons", 
+            "img5": JohnTurturro, 
+            "link5" : "https://pt.wikipedia.org/wiki/John_Turturro"
+        },
+        { 
+            "ator6" : "Anthony Hopkins", 
+            "personagem6": "como Sir Edmund Burton", 
+            "img6": AnthonyHopkins, 
+            "link6" : "https://pt.wikipedia.org/wiki/Anthony_Hopkins"
+        },
+        { 
+            "ator7" : "Jerrod Carmichael", 
+            "personagem7": "como Jimmy Jay", 
+            "img7": JerrodCarmichael, 
+            "link7" : "https://tfwiki.net/wiki/Jimmy_(TLK)"
+        },
+        { 
+            "ator8" : "Santiago Cabrera", 
+            "personagem8": "como Santos", 
+            "img8": SantiagoCabrera, 
+            "link8" : "https://pt.wikipedia.org/wiki/Santiago_Cabrera"
+        },
+        { 
+            "ator9" : "Glenn Morshower", 
+            "personagem9": "como General Morshower",  
+            "img9": GeneralMorshower, 
+            "link9" : "https://pt.wikipedia.org/wiki/Glenn_Morshower"
+        },
+        { 
+            "ator10" : "Stanley Tucci", 
+            "personagem10": "como Merlin", 
+            "img10": StanleyTucci, 
+            "link10" : "https://pt.wikipedia.org/wiki/Stanley_Tucci"
+        },
+    ]
+
     return (
         <div>
-            <Container backGroundCustom="back_groud" backGroundImgCustom="backgound_img_5">
-
-            </Container>
+            {filme.map((infor) => (
+                <Container key={infor.title} backGroundCustom="back_groud" backGroundImgCustom="backgound_img_5">
+                    <SwitchInfor 
+                        title={infor.title} 
+                        textSinopse={infor.textSinopse}
+                        linkTrailer="https://www.youtube.com/embed/bzKbEBPsmo0?si=keQw0BKLy3EemrTa" 
+                        atores={atores}
+                    />
+                    <Redes />
+                </Container>
+            ))}
         </div>
 
     )
--- src/pages/TransformersFour.js
+++ src/pages/TransformersFour.js
@@ -1,11 +1,104 @@
 import Container from "../components/Container"
+import Redes from "../components/layout/Redes"
+import SwitchInfor from "../components/layout/SwitchInfor"
+
+import MarkWahlberg from "../img/Atores/Mark-Wahlberg.jpeg"
+import NicolaPeltz from "../img/Atores/Nicola-Peltz.jpg"
+import JackReynor from "../img/Atores/Jack-Reynor.jpeg"
+import StanleyTucci from "../img/Atores/Stanley-Tucci.jpeg"
+import LiBingbing from "../img/Atores/Li-Bingbing.jpeg"
+import KelseyGrammer from "../img/Atores/Kelsey-Grammer.jpeg"
+import TitusWelliver from "../img/Atores/Titus-Welliver.jpeg"
+import ThomasLennon from "../img/Atores/Thomas-Lennon.webp"
+import SophiaMyles  from "../img/Atores/Sophia-Myles.jpeg"
+import TJMiller from "../img/Atores/T-J-Miller.jpeg"
 
 function TransformersFour() {
+    
+
+    const filme = [
+        { 
+            "title": "Transformers: A Era da Extinção",
+            "textSinopse": "Após a batalha entre os Autobots e os Decepticons, que arrasou Chicago, os gigantescos robôs alienígenas desapareceram. Atualmente, eles são caçados pelos humanos, que não desejam passar por apuros novamente. Porém, enquanto a humanidade tenta se recuperar dessa terrível batalha, uma nova ameaça paira sobre a Terra."
+        }
+    ]
+
+    const atores = [
+        { 
+            "ator1" : "Mark Wahlberg", 
+            "personagem1": "como Cade Yeager", 
+            "img1": MarkWahlberg, 
+            "link1" : "https://pt.wikipedia.org/wiki/Mark_Wahlberg"
+        },
+        { 
+            "ator2" : "Nicola Peltz", 
+            "personagem2": "como Tessa Yeager", 
+            "img2": NicolaPeltz, 
+            "link2" : "https://pt.wikipedia.org/wiki/Nicola_Peltz"
+        },
+        { 
+            "ator3" : "Jack Reynor", 
+            "personagem3": "como Shane", 
+            "img3": JackReynor, 
+            "link3" : "https://pt.wikipedia.org/wiki/Jack_Reynor"
+        },
+        { 
+            "ator4" : "Stanley Tucci", 
+            "personagem4": "como Joshua" , 
+            "img4": StanleyTucci, 
+            "link4" : "https://pt.wikipedia.org/wiki/Stanley_Tucci"
+        },
+        { 
+            "ator5" : "Li Bingbing", 
+            "personagem5": " como Su Yuerning", 
+            "img5": LiBingbing, 
+            "link5" : "https://pt.wikipedia.org/wiki/Li_Bingbing"
+        },
+        { 
+            "ator6" : "Kelsey Grammer", 
+            "personagem6": "como Harold Attinger", 
+            "img6": KelseyGrammer, 
+            "link6" : "https://pt.wikipedia.org/wiki/Kelsey_Grammer"
+        },
+        { 
+            "ator7" : "Titus Welliver", 
+            "personagem7": "como Savoy", 
+            "img7": TitusWelliver, 
+            "link7" : "https://pt.wikipedia.org/wiki/Titus_Welliver"
+        },
+        { 
+            "ator8" : "Thomas Lennon", 
+            "personagem8": "como Chief of Staff", 
+            "img8": ThomasLennon, 
+            "link8" : "https://pt.wikipedia.org/wiki/Thomas_Lennon"
+        },
+        { 
+            "ator9" : "Sophia Myles", 
+            "personagem9": "como Darcy", 
+            "img9": SophiaMyles, 
+            "link9" : "https://pt.wikipedia.org/wiki/Sophia_Myles"
+        },
+        { 
+            "ator10" : "T.J.Miller", 
+            "personagem10": "como Lucas", 
+            "img10": TJMiller, 
+            "link10" : "https://pt.wikipedia.org/wiki/T._J._Miller"
+        },
+    ]
+
     return (
         <div>
-            <Container backGroundCustom="back_groud" backGroundImgCustom="backgound_img_4">
-
-            </Container>
+            {filme.map((infor) => (
+                <Container key={infor.title} backGroundCustom="back_groud" backGroundImgCustom="backgound_img_4">
+                    <SwitchInfor 
+                        title={infor.title} 
+                        textSinopse={infor.textSinopse}
+                        linkTrailer="https://www.youtube.com/embed/BcVlQy5aUQg?si=-qylPPaqF3zy1ujj" 
+                        atores={atores}
+                    />
+                    <Redes />
+                </Container>
+            ))}
         </div>
 
     )
